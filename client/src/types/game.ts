7c988918b6841635180c08b4c82fb7538d06706c
doe1pq <tariqali30021@gmail.com>
2025-05-30 13:30:34 +0200

    hotfix for json file

--- client/src/types/game.ts
+++ client/src/types/game.ts
@@ -2,5 +2,29 @@ export interface Game {
   _id: string;
   name: string;
   createdAt: string;
-  tags?: string[]; 
+  tags?: string[];
+  maxPlayers?: number;
+  description?: string;
+  canvas?: any[];
+  gameFlow?: any[];
+  deck?: any[];
+  discardPile?: any[];
+  players?: any[];
+  initialHandCount?: number[];
+  shuffleOnStart?: boolean;
+  ruleSet?: {
+    id: string;
+    name: string;
+    gameplayMode: string;
+    maxPlayers: number;
+    teamCount: number;
+    playersPerTeam: number;
+    winConditions: any[];
+    eliminationConditions: any[];
+    actions: any[];
+    tags: string[];
+    turnEffects: any[];
+    initialHand: any[];
+    initialHandCount: number;
+  };
 }
